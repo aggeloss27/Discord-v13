516c2b1ccbf14dd20c6a7f9a4f1594b9c3e7646c
aggeloss27 <vergosan64@gmail.com>
2023-02-13 18:06:39 +0200

    modified:   commands/lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,31 +1,55 @@
 module.exports = {
   name: "lockdown",
   permissions: ["ADMINISTRATOR"],
-  aliases: [],
+  aliases: ["lockserver"],
   cooldown: 0,
   category: "utility",
   description: {
-    usage: ".lockdown",
-    content: "Locks down every channel on the server",
-    examples: [".lockdown"],
+    usage: ".lockdown <option>",
+    content:
+      "Locks down or unlocks every channel on the server (including vc's)",
+    examples: [".lockdown true", ".lockdown false"],
   },
   async execute(client, message, args, Discord, profileData) {
-    message.guild.channels.cache.forEach((channel) => {
-      try {
-        channel.updateOverwrite(
-          message.guild.roles.cache.find(
-            (e) => e.name.toLowerCase().trim() == "@everyone"
-          ),
-          {
-            SEND_MESSAGES: false,
-          }
-        );
-      } catch (e) {
-        console.log(e);
-        return console.log(`Couldn't lock ${channel}`);
-      }
-    });
+    if (!args[0]) return;
 
-    message.channel.send("Lockdown")
+    if (args[0] == "true") {
+
+      message.guild.channels.cache.forEach((channel) => {
+        try {
+          const x = message.guild.roles.cache.find(
+            (e) => e.name.toLowerCase().trim() == "@everyone"
+          );
+          channel.permissionOverwrites.edit(x, {
+            SEND_MESSAGES: false,
+            CONNECT: false,
+          });
+        } catch (e) {
+          console.log(e);
+          return console.log(`Couldn't lock ${channel}`);
+        }
+      });
+
+      message.channel.send("Lockdown");
+
+    } else if (args[0] == "false") {
+
+      message.guild.channels.cache.forEach((channel) => {
+        try {
+          const x = message.guild.roles.cache.find(
+            (e) => e.name.toLowerCase().trim() == "@everyone"
+          );
+          channel.permissionOverwrites.edit(x, {
+            SEND_MESSAGES: true,
+            CONNECT: true,
+          });
+        } catch (e) {
+          console.log(e);
+          return console.log(`Couldn't unlock ${channel}`);
+        }
+      });
+
+      message.channel.send("Lockdown is up");
+    }
   },
 };
